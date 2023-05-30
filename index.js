5946651eb1d5d0e50e47fbb83abd225fa4af9f0e
AymiManzanelli <amanzanelli6@gmail.com>
2023-05-30 20:00:22 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById("miBoton");
+
+function handleClick() {
+    alert("!Hola! Has hecho clic en el boton.");
+}
+
+miBoton.addEventListener("click", handleClick);
